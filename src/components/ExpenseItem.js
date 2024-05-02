35e31540593e4715bda4198e07d8ea138201a866
mipi1256 <mipi1258@gmail.com>
2024-05-02 11:58:41 +0900

    4. 컴포넌트 분할하기

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = ({title, price, date}) => {
 
   // 한자리 수를 두자리로 변환해주는 함수
   // To make the month and day to have a zero infront of it when the number is less than 2 digits.
   // Also, you need to receive the data into the function as a parameter.
+  /*
   const make2digit = (text) => {
     return text.toString().padStart(2, '0');
   }
+  */
 
-  // 날짜 포맷팅 변환 함수 정의
-  const makeFormattedDate = () => {
+  // 날짜 포맷팅 변환 함수 정의 -> ExpenseDate.js
+  /*const makeFormattedDate = () => {
     const year = date.getFullYear();
     const month = date.getMonth();
     const day = date.getDate();
 
     return `${year}년 ${make2digit(month)}월 ${make2digit(day)}일`;
   }
+  */
 
   // 숫자를 화폐 표기법으로 바꾸기
   // 숫자 콤마 찍기
@@ -26,7 +30,7 @@ const ExpenseItem = ({title, price, date}) => {
 
   return (
     <div className='expense-item'>
-      <div>{makeFormattedDate()}</div>
+      <ExpenseDate date={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>{formattedPrice} 원</div>
