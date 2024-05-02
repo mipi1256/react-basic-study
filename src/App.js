683efe2635341c13566781e3d6e95c59ac12e53c
mipi1256 <mipi1258@gmail.com>
2024-05-02 17:40:10 +0900

    10. 새 지출내역 추가 및 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Expenses from './components/Expense/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
@@ -32,17 +33,27 @@ function App() {
     },
   ];
 
+  // 지출 객체배열을 상태변수로 관리
+  const [expenseList, setExpenseList] = useState(expenses);
+
   // ExpenseFrom에게 내려보낼 함수
   const addExpenseHandler = (newExpense) => {
     console.log('App 컴포넌트에서 응답함!');
     console.log('newExpense: ', newExpense);
-    expenses.push(newExpense);
+
+    const modifyExpense = {
+      ...newExpense,
+      id: expenseList[expenseList.length - 1].id + 1,
+    };
+    console.log(modifyExpense);
+
+    setExpenseList([...expenseList, modifyExpense]);
   };
 
   return (
     <>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      <Expenses items={expenseList} />
     </>
   );
 }
