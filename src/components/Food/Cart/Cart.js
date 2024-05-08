9367bc9d871a4f8ff2994db4aa17e685d8855191
mipi1256 <mipi1258@gmail.com>
2024-05-08 17:17:43 +0900

    Cart 화면 +버튼 처리, Context 로직 수정

--- src/components/Food/Cart/Cart.js
+++ src/components/Food/Cart/Cart.js
@@ -4,21 +4,6 @@ import CartModal from '../../UI/Modal/CartModal';
 import CartContext from '../../store/cart-context';
 import CartItem from './CartItem';
 
-const DUMMY_CART = [
-  {
-    id: 'c1',
-    name: '스시',
-    amount: 2,
-    price: 46000,
-  },
-  {
-    id: 'c2',
-    name: '띠드버거',
-    amount: 1,
-    price: 12000,
-  },
-];
-
 const Cart = ({ onClose }) => {
   const {
     'cart-items': cartItemStyle,
